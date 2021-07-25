9733f24f5c040121cac7ea17007ba3fcaea40d76
xiayundong <1032176134@qq.com>
2021-07-25 17:46:13 +0800

    1.0.1-7
    添加睡眠感应界面

--- pages/smart/smart.js
+++ pages/smart/smart.js
@@ -1,26 +1,39 @@
 // pages/smart/smart.js
+const util = require('../../utils/util');
+const crcUtil = require('../../utils/crcUtil');
+const configManager = require('../../utils/configManager')
+const app = getApp();
+const preCMD = 'FFFFFFFF050000';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    connected: {}, // 当前连接的设备
+    skin: app.globalData.skin,
+    navbar: {
+      loading: false,
+      color: '#FFFFFF',
+      background: '#0A0A0C',
+      show: true,
+      animated: false,
+    },
+    openSmart:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let connected = configManager.getCurrentConnected();
+    this.setData({
+      skin: app.globalData.skin,
+      connected: connected
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
 
   /**
    * 生命周期函数--监听页面显示
@@ -29,12 +42,7 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
+  
 
   /**
    * 生命周期函数--监听页面卸载
@@ -43,24 +51,32 @@ Page({
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
+  /**
+   * 智能睡眠感应开关
+   */
+  smartSwitch:function() {
+    var connected = this.data.connected;
+    var openSmart = this.data.openSmart;
+    if(openSmart) {
+      // 发送关的命令
+      util.sendBlueCmd(connected,preCMD+'F03FD310');
+    } else {
+      // 发送开的命令
+      util.sendBlueCmd(connected,preCMD+'003F9710');
+    }
+    this.setData({
+      openSmart: !openSmart
+    })
   },
 
   /**
-   * 页面上拉触底事件的处理函数
+   * 跳转到睡眠报告页面
    */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  report:function() {
+    wx.navigateTo({
+      url: '/pages/smart/smart',
+    })
   }
+
 })

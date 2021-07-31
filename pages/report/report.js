3341d731794c618584465728410bee7385a1c1d2
xiayundong <1032176134@qq.com>
2021-07-31 20:35:03 +0800

    1.0.3-13
    添加曲线图功能

--- pages/report/report.js
+++ pages/report/report.js
@@ -5,8 +5,12 @@ const time = require('../../utils/time');
 const crcUtil = require('../../utils/crcUtil');
 const configManager = require('../../utils/configManager')
 const WxNotificationCenter = require('../../utils/WxNotificationCenter');
+const wxCharts = require('../../utils/wxcharts.js');
 const app = getApp();
 const preCMD = 'FFFFFFFF050000';
+
+var lineChart = null;
+
 Page({
 
   /**
@@ -50,7 +54,10 @@ Page({
       }
     })
     WxNotificationCenter.addNotification("BLUEREPLY", this.blueReply, this);
-    this.sendInitCmd();
+    // this.sendInitCmd();
+
+    this.onLoadlineChart();
+
   },
 
 
@@ -79,7 +86,7 @@ Page({
   sendInitCmd() {
     let cmd = 'FFFFFFFF0200030B01'
     let end = crcUtil.HexToCSU16(cmd);
-    util.sendBlueCmd(this.data.connected,cmd+end);
+    util.sendBlueCmd(this.data.connected, cmd + end);
   },
 
   /**
@@ -95,16 +102,74 @@ Page({
     }
 
     // 获取翻身次数
-    let fanshenNum  = util.str16To10(cmd.substr(24,2)); 
-    let pingtangTime = util.str16To10(cmd.substr(20,2));
-    let cetangTime = util.str16To10(cmd.substr(22,2));
+    let fanshenNum = util.str16To10(cmd.substr(24, 2));
+    let pingtangTime = util.str16To10(cmd.substr(20, 2));
+    let cetangTime = util.str16To10(cmd.substr(22, 2));
     let shuimianTime = parseInt(pingtangTime) + parseInt(cetangTime);
     this.setData({
-      timeShuimian:shuimianTime,
-      timePingtang:pingtangTime,
-      timeCetang:cetangTime,
-      timeFanshen:fanshenNum
+      timeShuimian: shuimianTime,
+      timePingtang: pingtangTime,
+      timeCetang: cetangTime,
+      timeFanshen: fanshenNum
     })
+  },
+
+
+
+  /**
+   * 加载曲线图
+   */
+  onLoadlineChart: function () {
+    let width = app.globalData.screenWidth;
+    console.info('onLoadlineChart->width', width);
+    var simulationData = this.createSimulationData();
+    lineChart = new wxCharts({
+      canvasId: 'lineCanvas',
+      type: 'line',
+      categories: simulationData.categories,
+      animation: true,
+      background: '#1E1F24',
+      series: [{
+          name: '翻身（次数）',
+          data: simulationData.data,
+          color:'#5EA2D7',
+          format: function (val, name) {
+            return val + '次';
+          }
+        }
+      ],
+      xAxis: {
+        disableGrid: true
+      },
+      yAxis: {
+        min: 0,
+        max: 25
+      },
+      width: width,
+      height: 250,
+      dataLabel: false,
+      dataPointShape: true,
+      legend:false,
+      extra: {
+        lineStyle: 'curve'
+      }
+    });
+  },
+
+
+
+  /**
+   * 初始化曲线图数据
+   */
+  createSimulationData: function () {
+    var categories = [];
+    var data = [];
+    categories = ['20:00', '22:00', '24:00', '02:00', '04:00', '06:00', '08:00'];
+    //data = [10, 15, 17, 18, 22, 20, 19]
+    return {
+      categories: categories,
+      data: data
+    }
   },
 
 

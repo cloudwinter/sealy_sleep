7b266fc46937a97d431e6ba4f7dfb149f256e80e
xiayundong <1032176134@qq.com>
2021-10-22 23:57:32 +0800

    1.0.4-4
    智能床的调整

--- component/tab/smartbed/smartbed.js
+++ component/tab/smartbed/smartbed.js
@@ -32,9 +32,10 @@ Component({
     startTime: '',
     endTime: '',
     hasSleepInduction: true,
-    zhinengShuimian:'00',
-    bbtzPotion:1,  // 背部调整位置
-    tbtzPotion:1,  // 背腿调整位置
+    zhinengShuimian: '00',
+    bbtzPotion: 1, // 背部调整位置 1，2，3 或者 3，2，1 初始化都是1
+    tbtzPotion: 1, // 背腿调整位置 1，2，3 或者 3，2，1 初始化都是1
+    animationStop: true, //停止动画
   },
 
 
@@ -180,7 +181,7 @@ Component({
 
     },
 
-    
+
 
 
 
@@ -257,7 +258,7 @@ Component({
       if (sleepPrefix == 'FFFFFFFF02000A14') {
         let zhinengShuimian = cmd.substr(32, 2);
         that.setData({
-          zhinengShuimian:zhinengShuimian
+          zhinengShuimian: zhinengShuimian
         })
       }
 
@@ -594,7 +595,7 @@ Component({
       var connected = this.data.connected;
       let zhinengShuimian = this.data.zhinengShuimian;
       let cmd = '';
-      if(zhinengShuimian == '01') {
+      if (zhinengShuimian == '01') {
         cmd = 'FFFFFFFF050000F03FD310'
         zhinengShuimian = '00';
       } else {
@@ -603,10 +604,181 @@ Component({
       }
       util.sendBlueCmd(connected, cmd);
       this.setData({
-        zhinengShuimian:zhinengShuimian
+        zhinengShuimian: zhinengShuimian
       })
       app.globalData.zhinengShuimian = zhinengShuimian;
 
+    },
+
+
+    /***************** 点击事件 */
+    /**
+     * 按下事件
+     * @param {*} e 
+     */
+    touchDHStart(e) {
+      this.startTime = e.timeStamp;
+      console.log("touchStart", this.startTime);
+      var type = e.currentTarget.dataset.type;
+      if (type) {
+        console.info('touchStart', type);
+        // 启动动画
+        this.setData({
+          animationStop: false
+        })
+      }
+      if (type == 'beibutzTop') {
+        this.donghua(true, this, 'beibutz');
+        this.tapBeibutz(true, true);
+      } else if (type == 'beibutzBottom') {
+        this.donghua(false, this, 'beibutz');
+        this.tapBeibutz(false, true);
+
+      } else if (type == 'tuibutzTop') {
+        this.donghua(true, this, 'tuibutz');
+        this.tapTuibutz(true, true);
+      } else if (type == 'tuibutzBottom') {
+        this.donghua(false, this, 'tuibutz');
+        this.tapTuibutz(false, true);
+      }
+    },
+    /**
+     * 抬起事件
+     * @param {*} e 
+     */
+    touchDHEnd(e) {
+      this.endTime = e.timeStamp;
+      console.log("touchEnd", this.endTime);
+      var type = e.currentTarget.dataset.type;
+      if (type) {
+        // 停止动画
+        console.info('touchEnd', type);
+        this.setData({
+          animationStop: true
+        })
+      }
+      // 还原动画
+      this.setData({
+        bbtzPotion: 1,
+        tbtzPotion: 1
+      });
+      if (type == 'beibutzTop') {
+        this.tapBeibutz(true, false);
+      } else if (type == 'beibutzBottom') {
+        this.tapBeibutz(false, false);
+      } else if (type == 'tuibutzTop') {
+        this.tapTuibutz(true, false);
+      } else if (type == 'tuibutzBottom') {
+        this.tapTuibutz(false, false);
+      }
+    },
+
+
+
+
+    /**
+     * 背部调整
+     * @param {*} top 上
+     * @param {*} start 按下
+     */
+    tapBeibutz(top, start) {
+      var cmd = '';
+      if (!start) {
+        // 松开 发停止码
+        cmd = '0000D700';
+        this.sendBlueCmd(cmd);
+      } else {
+        // 按下
+        if (top) {
+          cmd = '00039701';
+        } else {
+          cmd = '0004D6C3';
+        }
+        this.sendBlueCmd(cmd);
+      }
+    },
+
+
+
+
+
+    /**
+     * 腿部调整
+     * @param {*} top 上
+     * @param {*} start 按下
+     */
+    tapTuibutz(top, start) {
+      var cmd = '';
+      if (!start) {
+        // 松开 发停止码
+        cmd = '0000D700';
+        this.sendBlueCmd(cmd);
+      } else {
+        // 按下
+        if (top) {
+          cmd = '00065702';
+        } else {
+          cmd = '000796C2';
+        }
+        this.sendBlueCmd(cmd);
+      }
+    },
+
+
+
+    /********************动画处理 */
+    /**
+     * 顺序就是1，2，3，倒序就是3，2，1
+     * @param {是否倒序} reversal 
+     * @param type beibutz / tuibutz
+     */
+    donghua(reversal, cur, type) {
+      var that = cur;
+      console.info('donghua', that);
+      var stop = that.data.animationStop;
+      var position = 1;
+      if (type == 'beibutz') {
+        position = that.data.bbtzPotion;
+      } else {
+        position = that.data.tbtzPotion;
+      }
+      if (stop) {
+        // 还原初始值
+        if (type == 'beibutz') {
+          that.setData({
+            bbtzPotion: 1
+          })
+        } else {
+          that.setData({
+            tbtzPotion: 1
+          })
+        }
+        // 停止
+        return;
+      }
+      if (reversal) {
+        // 倒序
+        position--;
+        if (position <= 0) {
+          position = 3;
+        }
+      } else {
+        // 顺序
+        position++;
+        if (position >= 4) {
+          position = 1;
+        }
+      }
+      if (type == 'beibutz') {
+        that.setData({
+          bbtzPotion: position
+        });
+      } else {
+        that.setData({
+          tbtzPotion: position
+        });
+      }
+      setTimeout(that.donghua, 400, reversal, that, type);
     },
 
 

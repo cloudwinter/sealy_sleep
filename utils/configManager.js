7f3d5fe37bf9fbf20c51ef4652b76029812ca221
cloud <1032176134@qq.com>
2023-11-14 09:53:46 +0800

    1.0.8-4
    修改同步保存的bug

--- utils/configManager.js
+++ utils/configManager.js
@@ -245,10 +245,7 @@ function getAlarm(deviceId) {
  */
 function putKJAndAlarmType(type, deviceId) {
   let key = _TYPE_KJ_ALARM_KEY + deviceId;
-  wx.setStorageSync({
-    data: type,
-    key: key,
-  })
+  wx.setStorageSync(key,type);
 }
 
 /**
